0f64de679497036a28aef402b99d25a524791551
Guswojo-code <74015971+Guswojo-code@users.noreply.github.com>
2023-05-09 19:50:39 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', '194.15.36.14:3306');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '7hnf9VC1e2Lac7O4C0rK');
+define('DB_NAME', "LudusLogin");
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
+
+
+
